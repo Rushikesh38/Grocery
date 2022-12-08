1cd23ecff3010af1d5f28b1ff0c71fa2bf6e3513
Rushikesh38 <rushikeshsharma12@gmail.com>
2022-12-08 11:46:09 +0530

    paggination

--- client/src/Components/Menu/paggination.js
+++ client/src/Components/Menu/paggination.js
@@ -1,16 +1,19 @@
-const paggination=({totalpost,postperpage})=>{
+const Paggination=({totalpost,postperpage,pagginate})=>{
     const numbers =[];
     for(let i=1;i<=Math.ceil(totalpost/postperpage);i++){
         numbers.push(i);
     }   
+    console.log(numbers)
     return(
         <>
+        <h1>Paggination</h1>
             <ul>
                 {
                      numbers.map((num)=>{
                         return<>
                             <li>
-                                num
+                                <button onClick={()=>pagginate(num)}>{num}</button>
+                               
                             </li>
                         </>
                     })
@@ -19,4 +22,4 @@ const paggination=({totalpost,postperpage})=>{
         </>
     )
 }
-export default paggination;
+export default Paggination;

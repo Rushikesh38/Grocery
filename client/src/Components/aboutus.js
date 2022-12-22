60cfabace118c0e98040fc3a60c232cca1e7e849
Rushikesh38 <rushikeshsharma12@gmail.com>
2022-12-22 22:34:27 +0530

    about us

--- client/src/Components/aboutus.js
+++ client/src/Components/aboutus.js
@@ -1,9 +1,15 @@
 function contact() {
   return (
-    <div>
-      {/* <Navbar/> */}
-      <h1>About us</h1>
-    </div>
+    <>
+      <div>
+        <div className="container d-flex justify-content-center">
+          <div className="team">
+          <h4>About us</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere quis, ducimus suscipit ea nulla eum accusamus recusandae error laudantium, reprehenderit enim molestias reiciendis neque tempore autem rem assumenda consequuntur, minima laboriosam? Dolorum!</p>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
